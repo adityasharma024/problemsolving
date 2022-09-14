f7ba810b2e746f3853b826cf16db6fc3a5978f65
ADITYA SHARMA <91861881+adityasharma024@users.noreply.github.com>
2022-09-14 23:08:30 +0530

    Add files via upload

---
+++ pslooping2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    char ch;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            ch='A'+j-1;
+            
+            cout<<ch<<" ";
+            
+            
+            
+
+            
+            
+           
+            
+        }
+        
+        cout<<endl;
+    }
+   
+    
+}
